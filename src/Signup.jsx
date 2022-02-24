91d878f1341dc223c93b35f29d031fe81080b2fb
Surya Karan Sharma <suryakaran1234@gmail.com>
2022-02-25 00:00:19 +0530

    Formik and Yup Finished with useFormik Hook

---
+++ src/Signup.jsx
@@ -0,0 +1,80 @@
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import './Signup.css';
+
+function Signup() {
+  const formik = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+    },
+    validationSchema: Yup.object({
+      firstName: Yup.string()
+        .max(15, 'Must be 15 Character or less')
+        .required('Required'),
+      lastName: Yup.string()
+        .max(15, 'Must be 15 Character or less')
+        .required('Required'),
+      email: Yup.string()
+        .email()
+        .max(15, 'Must be 15 Character or less')
+        .required('Required'),
+    }),
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+  console.log(formik.touched);
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <div className='input-container'>
+        <input
+          id='firstName'
+          name='firstName'
+          type='text'
+          placeholder='First Name'
+          value={formik.values.firstName}
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+        />
+        {formik.touched.firstName && formik.errors.firstName ? (
+          <p>{formik.errors.firstName}</p>
+        ) : null}
+      </div>
+      <div className='input-container'>
+        <input
+          id='lastName'
+          name='lastName'
+          type='text'
+          placeholder='Last Name'
+          value={formik.values.lastName}
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+        />
+        {formik.touched.lastName && formik.errors.lastName ? (
+          <p>{formik.errors.lastName}</p>
+        ) : null}
+      </div>
+      <div className='input-container'>
+        <input
+          id='email'
+          name='email'
+          type='text'
+          placeholder='Email'
+          value={formik.values.email}
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+        />
+        {formik.touched.email && formik.errors.email ? (
+          <p>{formik.errors.email}</p>
+        ) : null}
+      </div>
+      <button type='submit'>Submit</button>
+    </form>
+  );
+}
+
+export default Signup;
